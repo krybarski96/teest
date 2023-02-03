b6f949ebd0a783d2f30a65d972e34ce8dfb9d4fa
Kamil R <87485867+krybarski96@users.noreply.github.com>
2023-02-03 16:00:15 +0100

    First

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+    return (
+        <div>
+            <h1 className="w-full text-2xl font-bold uppercase p-5">React!</h1>
+        </div>
+    )
+}
+
+export default Navbar
