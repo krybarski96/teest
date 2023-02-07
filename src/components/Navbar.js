a7223adaa4129c58e53b315474b57d964481e07d
Kamil R <87485867+krybarski96@users.noreply.github.com>
2023-02-07 13:46:34 +0100

    Wprowadzenie li

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,8 +2,14 @@ import React from "react";
 
 const Navbar = () => {
     return (
-        <div>
-            <h1 className="w-full text-2xl font-bold uppercase p-5">React! Wieszx jak jest</h1>
+        <div className="flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white">
+            <h1 className="basis-1/4 text-2xl font-bold uppercase p-5 text-main_blue">React!</h1>
+            <ul className="flex">
+                <li className="p-4">Strona główna</li>
+                <li className="p-4">Portfolio</li>
+                <li className="p-4">O mnie</li>
+                <li className="p-4">Kontakt</li>
+            </ul>
         </div>
     )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,21 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}",],
   theme: {
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
+    colors: {
+      "main_blue": '#00F6ED',
+      'second_mustard': '#FED766',
+      'akcent': '#FAF33E',
+      'black': '#070707',
+      'white': '#f9f9f9',
+      'gray': '#5F7470',
+    },
     extend: {},
   },
   plugins: [],
