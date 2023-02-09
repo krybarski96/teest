72b3794332aa492a8aa9524eeb044a2ce14e5d12
Kamil R <87485867+krybarski96@users.noreply.github.com>
2023-02-09 14:02:38 +0100

    druga zakładka

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import Analics from "./components/Analics";
 
 function App() {
   return (
     <div>
       <Navbar/>
       <Hero/>
+      <Analics/>
     </div>
   );
 }
